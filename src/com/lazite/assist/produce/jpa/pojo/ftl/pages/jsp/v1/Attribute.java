c409f4302b3abed7bce6397967e51369c7cdbf86
lazite <774029165@qq.com>
2016-12-09 19:03:07 +0800

    dsa

---
+++ src/com/lazite/assist/produce/jpa/pojo/ftl/pages/jsp/v1/Attribute.java
@@ -0,0 +1,16 @@
+package com.lazite.assist.produce.jpa.pojo.ftl.pages.jsp.v1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Attribute{
+	
+	private String name;
+	
+	private String nameCN;
+	
+	private String type;//属性类型,int,double,
+	
+	
+}
